18234d6c8203e68080e379f5a2ddf49ea821fabb
Lasse Gaardsholt <lasse.gaardsholt@gmail.com>
2020-07-05 12:08:19 +0200

    Added settings class, to read settings from env variables

---
+++ Settings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace busylight_server
+{
+    public class Settings
+    {
+        public static string API_KEYS { private set; get; }
+
+        public Settings()
+        {
+            foreach (var prop in this.GetType().GetProperties())
+                prop.SetValue(this, Environment.GetEnvironmentVariable(prop.Name));
+
+        }
+
+    }
+}
