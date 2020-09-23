af87103484f693e0201301150169723fb171444f
Lasse Gaardsholt <lasse.gaardsholt@gmail.com>
2020-09-24 00:22:16 +0200

    Added metrics to all endpoints

--- Controllers/BusyController.cs
+++ Controllers/BusyController.cs
@@ -12,8 +12,10 @@ namespace busylight_server.Controllers
     [Authorize]
     public class BusyController : ControllerBase
     {
-        private static readonly Counter _metricsMessagesReceived = Metrics.CreateCounter("myapp_requests_total", "Number of requests received, by HTTP method.");
-        
+        private static readonly Counter _metricsColor = Metrics.CreateCounter("color_requests_total", "Number of requests to change color.");
+        private static readonly Counter _metricsRing = Metrics.CreateCounter("ring_requests_total", "Number of requests to do a ring.");
+        private static readonly Counter _metricsPolice = Metrics.CreateCounter("police_requests_total", "Number of requests to call the Police.");
+
         private IHubContext<BusyHub> HubContext { get; set; }
         public BusyController(IHubContext<BusyHub> hubcontext) => HubContext = hubcontext;
 
@@ -21,19 +23,21 @@ namespace busylight_server.Controllers
         [HttpPost("{color}")]
         public async Task Color(string color, string group)
         {
+            _metricsColor.Inc();
             await HubContext.Clients.Group(group).SendAsync("SetColor", color);
         }
 
         [HttpPost("{group}")]
         public async Task Ring(string group)
         {
-            _metricsMessagesReceived.Inc();
+            _metricsRing.Inc();
             await HubContext.Clients.Group(group).SendAsync("Ring");
         }
 
         [HttpPost("{group}")]
         public async Task Police(string group)
         {
+            _metricsPolice.Inc();
             await HubContext.Clients.Group(group).SendAsync("Police");
         }
 
